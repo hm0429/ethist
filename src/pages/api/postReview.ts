a72d1946827725fbebd3a5938545206bf46f8055
Hideyoshi Moriya <hide@piyo.jp>
2023-11-19 00:49:02 +0300

    add test data to firestore

---
+++ src/pages/api/postReview.ts
@@ -0,0 +1,47 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { initializeApp, cert, getApps } from 'firebase-admin/app';
+import { getFirestore } from 'firebase-admin/firestore';
+
+const db = getFirestore();
+
+export const config = {
+    api: {
+      externalResolver: true,
+    },
+  };
+  
+  export type PostReviewReply = {
+    code: string;
+  };
+
+  
+  export default async function handler(
+    req: NextApiRequest,
+    res: NextApiResponse<PostReviewReply>
+  ) {
+    console.log("Received request to post review:\n", req.body);
+
+    const docRef = await db.collection('reviews').add({
+        placeId: 42,
+        title: "title",
+        body: "body"
+    });
+
+    res.status(200).send({
+        code: "success"
+    });
+  }
+
+
+
+
+if (!getApps()?.length) {
+    initializeApp({
+      credential: cert(
+        // 環境変数から認証情報を取得
+        JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string)
+      ),
+    });
+  }
+  
+  export const adminDB = getFirestore();
