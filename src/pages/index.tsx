22b0c9066b8df64c688ba5a92f23b1889deb5501
Hideyoshi Moriya <hide@piyo.jp>
2023-11-19 04:45:59 +0300

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -32,7 +32,11 @@ export default function Home() {
 
 	const onSuccess = (result: ISuccessResult) => {
 		// This is where you should perform frontend actions once a user has been verified, such as redirecting to a new page
-		window.alert("Successfully verified with World ID! Your nullifier hash is: " + result.nullifier_hash);
+		// window.alert("Successfully verified with World ID! Your nullifier hash is: " + result.nullifier_hash);
+
+		// TODO: show message
+		// reload
+
 	};
 
 	const handleProof = async (result: ISuccessResult) => {
