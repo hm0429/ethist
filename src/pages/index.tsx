f9eb28a74abb54c7d574122d241f791c4bfc5e7f
Hideyoshi Moriya <hide@piyo.jp>
2023-11-19 07:14:05 +0300

    add KEDI image

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,7 @@ import {
 	Input,
 	Checkbox,
 	Button,
-	Typography,
+	Typography
 } from "@material-tailwind/react";
 
 import Reviews from '../components/Reviews';
@@ -109,10 +109,23 @@ export default function Home() {
 					<div className="text-center">
 						<p className="text-4xl font-extrabold mb-4">HomoSapienSays</p>
 						<p>Please leave a review for THE KEDI HOTEL!</p>
-
+						<img
+							className="mt-4"
+							src="/images/kedi.png" 
+							alt="THE KEDI HOTEL"
+							width={400} 
+							height={400} 
+						/>
 					</div>
 
 					<form className="space-y-6" action="#" method="POST">
+						<input
+							ref={titleRef}
+							value={`Place ID: ${process.env.NEXT_PUBLIC_DEMO_PLACE_ID}`}
+							placeholder="Title"
+							className="w-full px-3 py-2 placeholder-gray-500 text-gray-900 bg-gray-50 rounded-md border border-gray-300 focus:outline-none focus:border-indigo-500 transition-colors"
+							disabled
+						/>
 						<input
 							ref={titleRef}
 							placeholder="Title"
